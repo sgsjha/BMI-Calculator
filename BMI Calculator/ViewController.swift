c9694b3e9f4f022c38a524b8c81df766cc95ef94
sgsjha <sarthakjhaa11@gmail.com>
2024-06-26 17:36:12 +0530

    basics done

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    @IBOutlet weak var demoLabel: UILabel!
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
     
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        demoLabel.text = "Project Updated !"
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func calculateBMI(_ sender: UIButton) {
+        guard let heightText = heightTextField.text, let heightCm = Double(heightText),
+              let weightText = weightTextField.text, let weight = Double(weightText) else {
+            resultLabel.text = "Please enter valid numbers."
+            return
+        }
+        
+        // Convert height from cm to m
+               let heightM = heightCm / 100
+               
+               // BMI formula: weight (kg) / (height (m) * height (m))
+               let bmi = weight / (heightM * heightM)
+               resultLabel.text = String(format: "Your BMI is %.2f", bmi)
+        
+    }
+    
+    
 }
-
