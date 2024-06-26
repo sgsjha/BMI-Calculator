44cfc340aee617fc3ad50b0937542d7d163921ba
sgsjha <sarthakjhaa11@gmail.com>
2024-06-26 16:52:34 +0530

    updated, test 1

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var demoLabel: UILabel!
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        demoLabel.text = "Project Updated !"
         // Do any additional setup after loading the view.
     }
 
